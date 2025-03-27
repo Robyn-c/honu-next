2a89c04bff073b2f5d77232f27bb284aea81aefa
Robyn Mac <scuadra02@gmail.com>
2025-03-27 19:32:33 -0300

    refactor: move hamburger menu to separate component

--- app/(home)/components/MobileNavbar.tsx
+++ app/(home)/components/MobileNavbar.tsx
@@ -1,34 +1,19 @@
-import { 
-  Sheet,
-  SheetTrigger,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetDescription
- } from '@/components/ui/sheet'
-import { Menu } from 'lucide-react';
+import MobileMenu from "@/app/(home)/components/MobileMenu";
 
 const MobileNavbar = () => {
   return (
     <>
-      <Sheet>
-        <SheetTrigger>
-          <Menu />
-        </SheetTrigger>
-        <SheetContent side='left'>
-          <SheetHeader>
-            <SheetTitle>Are you absolutely sure?</SheetTitle>
-            <SheetDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </SheetDescription>
-          </SheetHeader>
-        </SheetContent>
-      </Sheet>
       {/* 
         Hamburger menu 
           Nested drawer
             Links
+      */}
+      <MobileMenu />
+      {/*
+        TODO: LOGO
+        TODO: ACCOUNT ICONS
+
+        
       */}
     </>
   );
