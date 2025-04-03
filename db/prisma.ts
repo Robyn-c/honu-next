15859c3ff0836d31739185c9055fc9727b30de7c
Robyn Mac <scuadra02@gmail.com>
2025-04-02 21:06:48 -0300

    refactor: rewrite prisma to make a global prisma instance for optimization

--- db/prisma.ts
+++ db/prisma.ts
@@ -10,4 +10,9 @@ const connectionString = `${process.env.DATABASE_URL}`
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaNeon(pool)
-export const prisma = new PrismaClient({ adapter })
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
