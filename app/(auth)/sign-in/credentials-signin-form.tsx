8278b5ebc8e7f6edb2ede8d136ed34f798802335
Robyn Mac <scuadra02@gmail.com>
2025-04-05 19:09:41 -0300

    feat: add callback url after sign in

--- app/(auth)/sign-in/credentials-signin-form.tsx
+++ app/(auth)/sign-in/credentials-signin-form.tsx
@@ -1,13 +1,40 @@
 'use client';
+import Link from "next/link";
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
+import { useSearchParams } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import Link from "next/link";
+
+import { signInWithCredentials } from "@/lib/actions/user.actions";
+
+
 
 const CredentialsSignInForm = () => {
+  
+  const [data, action] = useActionState(signInWithCredentials, {
+    success: false,
+    message: ''
+  })  
+  
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get('callbackUrl') || '/';
+
+  const SignInButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className="w-full" variant='default'>
+        { pending ? 'Entrando...' : 'Entrar' }
+      </Button>
+    )
+
+
+  }
   return ( 
-      <form>
+      <form action={action}>
+        <input type="hidden" name="callbackUrl" value={callbackUrl}/>
         <div className="space-y-6">
           <div className="space-y-2">
             <Label htmlFor='email'>Email</Label>
@@ -18,8 +45,11 @@ const CredentialsSignInForm = () => {
             <Input id="password" name="password" type="password" required autoComplete='password'/>
           </div>
           <div>
-            <Button className="w-full" variant='default'>Entrar</Button>
+            <SignInButton />
           </div>
+          { data && !data.success && (
+            <p className="text-sm text-center text-destructive">{data.message}</p>
+          )}
           <div className="text-sm text-center text-muted-foreground">
             <Link href='/sign-up' target="_self" className="link">¿No tienes una cuenta? Regístrate</Link>
           </div>
--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -5,14 +5,30 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+
 import CredentialsSignInForm from "./credentials-signin-form";
+import { auth } from "@/auth";
 
 export const metadata : Metadata = {
   title: "Entrar"
 }
 
-const SignInPage = () => {
+const SignInPage = async(props: {
+  searchParams: Promise<{
+    callbackUrl: string
+  }>
+}) => {
+  const { callbackUrl } = await props.searchParams;
+  const session = await auth();
+  
+  if (session) {
+    return redirect(callbackUrl || '/');
+
+  }
+
   return ( 
     <section className="max-w-sm w-full">
       <Card>
