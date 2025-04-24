d037670e9fb965c72b6b63db1be9399a1b79e4ae
Robyn Mac <scuadra02@gmail.com>
2025-04-24 19:26:26 -0400

    feat: cart model and validators

--- lib/validators.ts
+++ lib/validators.ts
@@ -17,3 +17,23 @@ export const signUpFormSchema = z.object({
   message: "Password don't match", 
   path: ['confirmPassword']
 });
+
+// Cart Schema
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, 'Producto es requerido'),
+  name: z.string().min(1, 'Nombre es requerido'),
+  slug: z.string().min(1, 'Slug es requerido'),
+  qty: z.number().int().nonnegative('La cantidad debe ser un número positivo'),
+  image: z.string().min(1, 'Imagen es requerida'),
+  price: z.number().int().nonnegative('El precio no puede ser negativo')
+});
+
+// TODO : Pricing checkup
+export const insertCartSchema = z.object({
+  items: z.array(cartItemSchema),
+  itemsPrice: z.number().int(),
+  totalPrice: z.number().int(),
+  shippingPrice: z.number().int(),
+  sessionCartId: z.string().min(1, 'Id de session requerido'),
+  userId: z.string().optional().nullable(),
+});
